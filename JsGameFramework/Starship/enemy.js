577d99354235ab7cc92c402b71c02133e505e1b7
srcemil <57904112+srcemil@users.noreply.github.com>
2020-06-05 03:06:03 +0200

    kommentarer

--- JsGameFramework/Starship/enemy.js
+++ JsGameFramework/Starship/enemy.js
@@ -1,8 +1,8 @@
 class enemy {
   constructor(x, y){
-    this.HP = 100; // Hälsa på fienden
-    this.x = x; // x värden på fienden
-    this.y = y; // y värde på fienden
+    this.HP = 100;                    // Hälsa på fienden
+    this.x = x;                       // x värden på fienden
+    this.y = y;                       // y värde på fienden
   }
   update() {
   if(this.x > theShip.x){
@@ -11,18 +11,18 @@ class enemy {
   if(this.x < theShip.x){
       this.x -= 2;
     }
-  if(this.x - 20 > 1){ // Kollar om skeppets x värde är mer än 35
-      this.x -= 4;      // Förflytta skeppet åt vänster
+  if(this.x - 20 > 1){                // Kollar om fiendens x värde är mer än 1
+      this.x -= 4;                    // Förflytta fienden åt vänster
     }
-  if(this.x + 20 < gGameWidth - 50){ // Kollar om skeppets x är mer än canvas bredden
-      this.x += 4;									// Förflytta skeppet åt höger
+  if(this.x + 20 < gGameWidth - 50){  // Kollar om fienden x är mer än canvas bredden
+      this.x += 4;									  // Förflytta fienden åt höger
     }
   }
   draw(ctx) {
-    ctx.beginPath(); // Började rita
-    ctx.fillStyle = "blue"; // Färgen är blå
+    ctx.beginPath();                  // Började rita
+    ctx.fillStyle = "blue";           // Färgen är blå
     ctx.rect(this.x, this.y, 50, 50); // Ritar ut en kvadrat som med x, y värde och storlek vilket är 50px x 50px
-    ctx.closePath(); // Slutar rita
-    ctx.fill(); // Fyller kvadraten med den färg vi valt
+    ctx.closePath();                  // Slutar rita
+    ctx.fill();                       // Fyller kvadraten med den färg vi valt
   }
 }
